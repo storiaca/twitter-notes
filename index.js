8124119e8e889c90546c6ffd685ebb21a1df121e
storiaca <storiaca@gmail.com>
2017-05-27 15:32:49 +0200

    Adding oauth

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
+var url = require('url');
 var express = require('express');
+var authenticator = require('./authenticator');
+var config = require('./config');
 var app = express();
 
-app.get("/", function(req, res){
-    res.send("ho ho ho");
+// Add cookie parsing functionality to our Express app
+app.use(require('cookie-parser')());
+
+// Take user to Twitter's login page
+app.get('/auth/twitter', authenticator.redirectToTwitterLoginPage);
+
+// This is the callback url that the user is redirected to after signing in
+app.get(url.parse(config.oauth_callback).path, function(req, res) {
+
+    authenticator.authenticate(req, res, function(err){
+        if(err) {
+            console.log(err);
+            res.sendStatus(401);
+        } else {
+            res.send("Authentication Successful");
+        }
+    });
 });
 
-app.listen(8080, function(){
-    console.log("Express server is running on port 8080");
-})
+// Start listening for requests
+app.listen(config.port, function() {
+    console.log("Listening on port " + config.port);
+});
